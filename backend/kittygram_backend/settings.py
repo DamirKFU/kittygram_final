2e8c2882a3563cc98ea452e96ef6baadfc337c1a
JavaFix <Damir.SakhbievGG@gmail.com>
2026-02-19 16:01:14 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,9 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
-DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 ALLOWED_HOSTS = ['158.160.225.164', '127.0.0.1', 'localhost', 'kittygramapp.space']
 
